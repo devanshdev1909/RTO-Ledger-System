bce1c9f75d22bc851096648757d632f58f89e99d
Ehtesham <ehteshamiraqui2@gmail.com>
2026-06-10 17:17:15 +0530

    added new customer form

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -5,4 +5,10 @@ const customerController = require("../controllers/customer.controller");
 // Show customers page
 router.get("/", customerController.renderCustomersPage);
 
+//Show form 
+router.get("/new", customerController.showNewCustomerForm);
+
+//Save customer
+router.post("/", customerController.createCustomer);
+
 module.exports = router;
