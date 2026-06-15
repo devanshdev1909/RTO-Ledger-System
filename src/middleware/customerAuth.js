1e5c70d53b29e7cbe57849d209d1fa763781e26a
Ehtesham <ehteshamiraqui2@gmail.com>
2026-06-15 17:25:41 +0530

    feat: implement customer management module with CRUD controllers and EJS views

--- src/middleware/customerAuth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.session && req.session.customerId) {
-        // Expose customer details to EJS templates
-        res.locals.customerName = req.session.customerName;
-        return next();
-    }
-    res.redirect('/login');
-};
--- src/models/Customer.js
+++
@@ -1,7 +0,0 @@
-const pool = require("../config/db");
-
-class Customer {
-    // Model methods will be written here
-}
-
-module.exports = Customer;
